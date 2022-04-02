5490f07ed47c79b4f8a356f87c30ada893d665bb
NoobHereo <NoobHereo@users.noreply.github.com>
2022-04-02 11:55:51 +0200

    Basic player script & assets

---
+++ Assets/Scripts/GameCamera.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class GameCamera : MonoBehaviour
+{
+    public Vector3 Offset = new Vector3(0, 0, 0);
+    public Transform target { get ; private set; }
+    private bool targetSet = false;
+
+    private void Start()
+    {
+        target = GameObject.Find("Player").transform;
+        if (target != null)
+            targetSet = true;
+        else
+            targetSet = false;
+    }
+
+    private void LateUpdate()
+    {
+        if (targetSet)
+        {
+            transform.position = target.position + Offset;
+        }
+    }
+
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float Speed = 300f;
+    private Rigidbody2D rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        var horizontal = Input.GetAxisRaw("Horizontal");
+        var vertical = Input.GetAxisRaw("Vertical");
+
+        rb.velocity = new Vector2(horizontal * Speed * Time.fixedDeltaTime, vertical * Speed * Time.fixedDeltaTime);
+    }
+
+}
