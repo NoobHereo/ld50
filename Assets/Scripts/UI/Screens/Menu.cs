b9b59e1b0d8510a2a5b12b1ae3ebb8cf1266ac1b
NoobHereo <NoobHereo@users.noreply.github.com>
2022-04-02 20:50:41 +0200

    1280x720 ui

---
+++ Assets/Scripts/UI/Screens/Menu.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Menu : MonoBehaviour
+{
+    public Button Playbutton;
+
+    private void Start()
+    {
+        Playbutton.onClick.AddListener(OnPlayClick);
+    }
+
+    private void OnPlayClick()
+    {
+        // Start the game
+    }
+}
