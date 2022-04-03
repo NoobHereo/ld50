3c61a17810d780f5d3d692475f2a183aa00be1f1
NoobHereo <NoobHereo@users.noreply.github.com>
2022-04-03 20:33:44 +0200

    Update PlayerSword.cs

--- Assets/Scripts/Objects/PlayerSword.cs
+++ Assets/Scripts/Objects/PlayerSword.cs
@@ -9,10 +9,7 @@ public class PlayerSword : MonoBehaviour
         {
             case PlayerSpriteState.Right:
             case PlayerSpriteState.IdleRight:
-                float x = transform.parent.position.x + 0.8f;
-                float y = transform.parent.position.y + 0.18f;
-
-                var pos = new Vector3(x, y, 0);
+                var pos = new Vector3(0.8f, 0.3f, 0);
                 var rot = Quaternion.Euler(0, 0, -45f);
 
                 SetOrientation(pos, rot);
@@ -21,10 +18,7 @@ public class PlayerSword : MonoBehaviour
 
             case PlayerSpriteState.Left:
             case PlayerSpriteState.IdleLeft:
-                float x2 = transform.parent.position.x + -0.8f;
-                float y2 = transform.parent.position.y + 0.7f;
-
-                var pos2 = new Vector3(x2, y2, 0);
+                var pos2 = new Vector3(-0.8f, 0.7f, 0);
                 var rot2 = Quaternion.Euler(0, 0, 135f);
 
                 SetOrientation(pos2, rot2);
@@ -33,10 +27,7 @@ public class PlayerSword : MonoBehaviour
 
             case PlayerSpriteState.Up:
             case PlayerSpriteState.IdleUp:
-                float x3 = transform.parent.position.x + 0.2f;
-                float y3 = transform.parent.position.y + 1.3f;
-
-                var pos3 = new Vector3(x3, y3, 0);
+                var pos3 = new Vector3(0.33f, 1.3f, 0);
                 var rot3 = Quaternion.Euler(0, 0, 45f);
 
                 SetOrientation(pos3, rot3);
@@ -45,10 +36,7 @@ public class PlayerSword : MonoBehaviour
 
             case PlayerSpriteState.Down:
             case PlayerSpriteState.IdleDown:
-                float x4 = transform.parent.position.x + -0.3f;
-                float y4 = transform.parent.position.y + -0.3f;
-
-                var pos4 = new Vector3(x4, y4, 0);
+                var pos4 = new Vector3(-0.3f, -0.3f, 0);
                 var rot4 = Quaternion.Euler(0, 0, -135f);
 
                 SetOrientation(pos4, rot4);
@@ -58,7 +46,7 @@ public class PlayerSword : MonoBehaviour
 
     private void SetOrientation(Vector3 pos, Quaternion rot)
     {
-        transform.position = pos;
+        transform.position = transform.parent.position + pos;
         transform.rotation = rot;
     }
 
