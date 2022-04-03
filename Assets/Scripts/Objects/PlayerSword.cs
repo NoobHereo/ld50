e094ceb9cec8f8372c461d9988ebab5d7be4ed2f
NoobHereo <NoobHereo@users.noreply.github.com>
2022-04-03 20:31:13 +0200

    player animation and weapon

---
+++ Assets/Scripts/Objects/PlayerSword.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+
+public class PlayerSword : MonoBehaviour
+{
+
+    public void UpdateOrientation(PlayerSpriteState state)
+    {
+        switch(state)
+        {
+            case PlayerSpriteState.Right:
+            case PlayerSpriteState.IdleRight:
+                float x = transform.parent.position.x + 0.8f;
+                float y = transform.parent.position.y + 0.18f;
+
+                var pos = new Vector3(x, y, 0);
+                var rot = Quaternion.Euler(0, 0, -45f);
+
+                SetOrientation(pos, rot);
+                break;
+
+
+            case PlayerSpriteState.Left:
+            case PlayerSpriteState.IdleLeft:
+                float x2 = transform.parent.position.x + -0.8f;
+                float y2 = transform.parent.position.y + 0.7f;
+
+                var pos2 = new Vector3(x2, y2, 0);
+                var rot2 = Quaternion.Euler(0, 0, 135f);
+
+                SetOrientation(pos2, rot2);
+                break;
+
+
+            case PlayerSpriteState.Up:
+            case PlayerSpriteState.IdleUp:
+                float x3 = transform.parent.position.x + 0.2f;
+                float y3 = transform.parent.position.y + 1.3f;
+
+                var pos3 = new Vector3(x3, y3, 0);
+                var rot3 = Quaternion.Euler(0, 0, 45f);
+
+                SetOrientation(pos3, rot3);
+                break;
+
+
+            case PlayerSpriteState.Down:
+            case PlayerSpriteState.IdleDown:
+                float x4 = transform.parent.position.x + -0.3f;
+                float y4 = transform.parent.position.y + -0.3f;
+
+                var pos4 = new Vector3(x4, y4, 0);
+                var rot4 = Quaternion.Euler(0, 0, -135f);
+
+                SetOrientation(pos4, rot4);
+                break;
+        }
+    }
+
+    private void SetOrientation(Vector3 pos, Quaternion rot)
+    {
+        transform.position = pos;
+        transform.rotation = rot;
+    }
+
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,17 +3,44 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public float Speed = 300f;
+    public PlayerSword Sword;
+
     private Rigidbody2D rb;
+    private PlayerAnimator animator;
+    private PlayerSpriteState lastDir;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<PlayerAnimator>();
     }
 
     private void Update()
     {
         var horizontal = Input.GetAxisRaw("Horizontal");
         var vertical = Input.GetAxisRaw("Vertical");
+        var horizontalAbs = Mathf.Abs(horizontal);
+        var verticalAbs = Mathf.Abs(vertical);
+
+        if (horizontalAbs < 0.1f && verticalAbs < 0.1f)
+        {
+            animator.UpdateSpriteState(lastDir);
+            rb.velocity = Vector3.zero;
+            Sword.UpdateOrientation(lastDir);
+        }
+
+        if (horizontalAbs > verticalAbs)
+        {
+            animator.UpdateSpriteState(horizontal > 0.1f ? PlayerSpriteState.Right : PlayerSpriteState.Left);
+            lastDir = horizontal > 0.1f ? PlayerSpriteState.IdleRight : PlayerSpriteState.IdleLeft;
+            Sword.UpdateOrientation(horizontal > 0.1f ? PlayerSpriteState.Right : PlayerSpriteState.Left);
+        }
+        else if (verticalAbs > horizontalAbs)
+        {
+            animator.UpdateSpriteState(vertical > 0.1f ? PlayerSpriteState.Up : PlayerSpriteState.Down);
+            lastDir = vertical > 0.1f ? PlayerSpriteState.IdleUp : PlayerSpriteState.IdleDown;
+            Sword.UpdateOrientation(vertical > 0.1f ? PlayerSpriteState.Up : PlayerSpriteState.Down);
+        }
 
         rb.velocity = new Vector2(horizontal * Speed * Time.fixedDeltaTime, vertical * Speed * Time.fixedDeltaTime);
     }
