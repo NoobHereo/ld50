776274e8229ad7d012ca221adc4d0827b7ec560b
NoobHereo <NoobHereo@users.noreply.github.com>
2022-04-03 19:16:51 +0200

    Title screen & editor update

--- Assets/Scripts/UI/Screens/Menu.cs
+++ Assets/Scripts/UI/Screens/Menu.cs
@@ -4,12 +4,13 @@ using UnityEngine.SceneManagement;
 
 public class Menu : MonoBehaviour
 {
-    public Button Playbutton, EditorButton;
+    public Button Playbutton, EditorButton, QuitButton;
 
     private void Start()
     {
         Playbutton.onClick.AddListener(OnPlayClick);
         EditorButton.onClick.AddListener(OnEditorClick);
+        QuitButton.onClick.AddListener(OnQuitClick);
     }
 
     private void OnPlayClick()
@@ -26,5 +27,10 @@ public class Menu : MonoBehaviour
     private void OnEditorClick()
     {
         SceneManager.LoadScene("MapEditor");
+    }
+
+    private void OnQuitClick()
+    {
+        Application.Quit();
     }
 }
